540e89155fb3027f417139bb94c5073140f37d08
Rajat Verma <rajatv785v@gmail.com>
2022-11-03 01:37:51 +0530

    code

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-// ===> Create Local Server
+//  Create Local Server ==>
+
 // const http=require('http');
 // http.createServer((req,resp)=>{
 //   resp.write("<h1>Hello world</h1>");
 //   resp.end();
 // }).listen(5000)
 
+// Use a package of color ==>
 
-const colors=require('colors')
-console.log("package".bgblue)
+// const colors=require('colors')
+// console.log("package".bgblue)
+
