33d60deb763c31a2eb0bfb4ba56f45e7749849e0
Rajat Verma <rajatv785v@gmail.com>
2022-11-03 19:56:28 +0530

    code

---
+++ api.js
@@ -0,0 +1,27 @@
+const data=[
+    
+    {name:'rajat verma',role:'rontend developer',sallery:'47 LPA',address:'Shahjahanpur'},
+    {name:'sumit verma',role:'backend developer',sallery:'17 LPA',address:'Shahjahanpur'},
+    {name:'rajan verma',role:'rontend developer',sallery:'27 LPA',address:'powayana'},
+    {name:'arnav verma',role:'full stack  developer',sallery:'6 LPA',address:'Shahjahanpur'},
+    {name:'saurabh verma',role:'rontend developer',sallery:'8 LPA',address:'muradpur'},
+    {name:'prashant verma',role:'backend developer',sallery:'9 LPA',address:'Shahjahanpur'},
+    {name:'shivm verma',role:'MERN stackdeveloper',sallery:'11 LPA',address:'Shahjahanpur'},
+    {name:'shivm verma',role:'MERN stackdeveloper',sallery:'11 LPA',address:'Shahjahanpur'},
+    {name:'shivm verma',role:'MERN stackdeveloper',sallery:'11 LPA',address:'Shahjahanpur'},
+    {name:'shivm verma',role:'MERN stackdeveloper',sallery:'11 LPA',address:'Shahjahanpur'},
+    {name:'rajat verma',role:'rontend developer',sallery:'47 LPA',address:'Shahjahanpur'},
+    {name:'sumit verma',role:'backend developer',sallery:'17 LPA',address:'Shahjahanpur'},
+    {name:'rajan verma',role:'rontend developer',sallery:'27 LPA',address:'powayana'},
+    {name:'arnav verma',role:'full stack  developer',sallery:'6 LPA',address:'Shahjahanpur'},
+    {name:'saurabh verma',role:'rontend developer',sallery:'8 LPA',address:'muradpur'},
+    {name:'prashant verma',role:'backend developer',sallery:'9 LPA',address:'Shahjahanpur'},
+    {name:'shivm verma',role:'MERN stackdeveloper',sallery:'11 LPA',address:'Shahjahanpur'},
+    {name:'shivm verma',role:'MERN stackdeveloper',sallery:'11 LPA',address:'Shahjahanpur'},
+    {name:'shivm verma',role:'MERN stackdeveloper',sallery:'11 LPA',address:'Shahjahanpur'},
+    {name:'shivm verma',role:'MERN stackdeveloper',sallery:'11 LPA',address:'Shahjahanpur'},
+    {name:'shivm verma',role:'MERN stackdeveloper',sallery:'11 LPA',address:'Shahjahanpur'},
+    {name:'shivm verma',role:'MERN stackdeveloper',sallery:'11 LPA',address:'Shahjahanpur'},
+];
+module.exports=data;
+ 
--- index.js
+++ index.js
@@ -1,13 +1,70 @@
-//  Create Local Server ==>
-
+//  CREATE LOCAL SERVER ================================================================================
 // const http=require('http');
 // http.createServer((req,resp)=>{
 //   resp.write("<h1>Hello world</h1>");
 //   resp.end();
 // }).listen(5000)
 
-// Use a package of color ==>
-
+// USE APACKAGE FOR COLOR ==>
 // const colors=require('colors')
 // console.log("package".bgblue)
 
+
+// INSTALL NODEMON GLOBALLY ===========================================================================
+// console.warn("Try Nodemon start with update ");
+
+
+// CREATE BASIC API=====================================================================================
+// const api=require('./api')
+// const http=require('http');
+// http.createServer((req,resp)=>{
+// resp.writeHead(200,{'Content-Type':'application/json'});
+// resp.write(JSON.stringify(api));
+// resp.end();
+// }).listen(4000)
+
+
+// INPUT FROM COMMAND LINE ===========================================================================
+// console.log(process.argv)
+
+// CREATE A FILE=======================================================================================
+// const fs=require('fs');
+// const file=process.argv
+// fs.writeFileSync(file[2],file[3]);
+
+// REMOVE OR ADD A FILE ================================================================================
+// const fs=require('fs');
+// const input=process.argv;
+
+// if(input[2]=='add'){
+//     fs.writeFileSync(input[3],input[4]);
+// }else if(input[2]=='remove'){
+//     fs.unlinkSync(input[3])
+// }else{
+//     console.log("invalid input");
+// }
+
+// CREATE FILE IN A FOLDER USING LOOP ==================================================================
+// const fs=require('fs');
+// const path=require('path');
+
+// const pathdir=path.join(__dirname,'files');
+
+// for(let i=0;i<5;i++){
+//  fs.writeFileSync(`${pathdir}/hello${i}.txt`,"my name is rajat")
+// }
+
+
+// READ AND LISTING FILE================================================================================
+//  const fs=require('fs');
+//  const path=require('path');
+
+//  const dirpath=path.join(__dirname,'files');
+//  fs.readdir(dirpath,(err,files)=>{
+//   files.map((item)=>{
+//   console.log(item)
+//   })
+//  })
+
+
+//
